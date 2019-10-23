e62f72086055a98bd075fc03258c4998e85cd307
Albin Gudmundsson <albin.gudmundsson@gmail.com>
2019-10-23 12:58:27 +0200

    method now generates a list with columns/rows, with random numbers

--- src/se/nackademin/Main.java
+++ src/se/nackademin/Main.java
@@ -15,16 +15,31 @@ public class Main {
 
         for (int i = 0; i < testGame.size(); i++) {
             if (i % c == 0) System.out.println();
-            System.out.print(testGame.get(i));
+            System.out.printf("%4d",testGame.get(i) );
         }
 
     }
 
     public static List<Integer> genGame (int columns, int rows) {
+        int[] numbers = new int[columns*rows];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+        }
+
+        Random r = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int temp = numbers[i];
+            int roll = r.nextInt(numbers.length);
+            numbers[i] = numbers[roll];
+            numbers[roll] = temp;
+        }
+
         List<Integer> game = new ArrayList<>();
 
         for (int i = 0; i < (columns*rows); i++) {
-            game.add(0);
+            game.add(numbers[i]);
         }
 
         return game;
